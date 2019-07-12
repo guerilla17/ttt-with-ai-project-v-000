0ccad4e8f5b8cc36ef38ad9a504265c14eb89d57
Gigi M <mmgamao1@gmail.com>
2019-07-12 17:39:27 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,7 @@ class Game
 
 # A game has two Players stored in a player_1 and player_2 property.
 
-  def initialize(player_1 = Player.new("X"), player_2 = Player.new("O"), board = )
+  def initialize(player_1 = Player.new("X"), player_2 = Player.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board 
